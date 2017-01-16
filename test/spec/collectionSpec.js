9efc9ed361ae190ce6797478fead5aebc27c5494
Nico Rehwaldt <git_nikku@nixis.de>
2017-01-16 14:21:33 +0100

    feat(collection): allow to add element at idx

--- test/spec/collectionSpec.js
+++ test/spec/collectionSpec.js
@@ -44,6 +44,89 @@ describe('collection api', function() {
 
       // given
       var a = {};
+      var b = { id: 'B' };
+      var c = { id: 'C' };
+
+      refs.bind(a, 'foos');
+
+      // when
+      a.foos.add(b);
+      a.foos.add(c);
+
+      // then
+      expect(a.foos).to.eql([ b, c ]);
+    });
+
+
+    it('should add at idx', function() {
+
+      // given
+      var a = {};
+      var b = { id: 'B' };
+      var c = { id: 'C' };
+      var d = { id: 'D' };
+
+      refs.bind(a, 'foos');
+
+      a.foos.add(b);
+      a.foos.add(c);
+
+      // when
+      a.foos.add(d, 1);
+
+      // then
+      expect(a.foos).to.eql([ b, d, c ]);
+    });
+
+
+    it('should move to front with idx', function() {
+
+      // given
+      var a = {};
+      var b = { id: 'B' };
+      var c = { id: 'C' };
+      var d = { id: 'D' };
+
+      refs.bind(a, 'foos');
+
+      a.foos.add(b);
+      a.foos.add(c);
+      a.foos.add(d);
+
+      // when
+      a.foos.add(c, 0);
+
+      // then
+      expect(a.foos).to.eql([ c, b, d ]);
+    });
+
+
+    it('should move to back with idx', function() {
+
+      // given
+      var a = {};
+      var b = { id: 'B' };
+      var c = { id: 'C' };
+      var d = { id: 'D' };
+
+      refs.bind(a, 'foos');
+
+      a.foos.add(b);
+      a.foos.add(c);
+      a.foos.add(d);
+
+      // when
+      a.foos.add(b, 2);
+
+      // then
+      expect(a.foos).to.eql([ c, d, b ]);
+    });
+
+
+    it('should inverse add', function() {
+
+      // given
+      var a = {};
       var b = {};
 
       refs.bind(a, 'foos');
@@ -52,7 +135,7 @@ describe('collection api', function() {
       a.foos.add(b);
 
       // then
-      expect(a.foos.contains(b)).to.equal(true);
+      expect(b.bar).to.equal(a);
     });
 
 
@@ -72,6 +155,7 @@ describe('collection api', function() {
     });
 
   });
+
 
   describe('#remove', function() {
 
